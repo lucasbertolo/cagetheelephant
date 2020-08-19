914a1e5767e17e62b2040359211ef94ecd6cbe7f
lucasbertolo <lucasbertolo2@gmail.com>
2020-08-19 14:37:22 -0300

    Adjusting dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,40 @@
 const path = require('path');
 
+const outputDir = './dist';
+
 module.exports = {
-  mode: 'production',
-  entry: './src/scripts/main.js',
-  resolve: {
-    extensions: ['.js'],
-  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: ['./src/scripts/main.js', './src/stylesheets/main.scss'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, outputDir),
+    filename: '[name].js',
+    publicPath: '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].css',
+            },
+          },
+          {
+            loader: 'extract-loader',
+          },
+          {
+            loader: 'css-loader?-url',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
   },
 };
